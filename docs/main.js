15425f12b1978367c0c155621b4091c277bf27ad
scarlett0714 <tpttyakfn03@naver.com>
2025-08-05 17:30:37 -0400

    index

--- docs/main.js
+++ docs/main.js
@@ -7,26 +7,8 @@ window.$docsify = {
       '/ko/_sidebar.md': 'ko/_sidebar.md'
     },
     toc: {
-      scope: '.markdown-section',
-      headings: 'h2, h3',
-      title: ''
-    },
-    plugins: [
-      function (hook) {
-        hook.doneEach(function () {
-          // 페이지 렌더 완료 후 실행됨
-          const tocTrigger = document.querySelector('.toc-hover-trigger');
-          const tocLinks = document.querySelectorAll('.page_toc a');
-          const count = tocLinks.length;
-  
-          tocTrigger.innerHTML = '';
-          for (let i = 0; i < count; i++) {
-            const bar = document.createElement('div');
-            bar.className = 'hover-bar';
-            tocTrigger.appendChild(bar);
-          }
-        });
-      }
-    ]
+      tocMaxLevel: 3,
+      target: 'h2, h3'
+    }
   };
   
